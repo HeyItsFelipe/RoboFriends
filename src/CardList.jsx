5dd621d8289d6ae668914c29a422aa9f4081d8fb
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-10-16 13:21:01 -0700

    Adds Card and CardList components.

---
+++ src/CardList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    const cardsArray = robots.map((robot) => {
+        let { id, name, email } = robot;
+        return (<Card key={id} id={id} name={name} email={email} />);
+    });
+    return (
+        <div>
+            {cardsArray}
+        </div>
+
+    )
+};
+export default CardList;
